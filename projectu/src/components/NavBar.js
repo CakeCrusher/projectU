04e5b7c48c52f0a94f23a39b0f021554cf6931b0
CakeCrusher <37946988+CakeCrusher@users.noreply.github.com>
2020-03-03 22:28:04 -0500

    Stylized search bar

--- projectu/src/components/NavBar.js
+++ projectu/src/components/NavBar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import UserContainer from './user/UserContainer'
 import logoIcon from '../static/svg/logoW.svg'
-import searchIcon from '../static/svg/searchW.svg'
+import searchIcon from '../static/svg/searchB.svg'
 import { Link } from 'react-router-dom'
 
 const NavBar = (props) => {
@@ -16,11 +16,13 @@ const NavBar = (props) => {
         <div className="nav-bar-container">
             <Link to="/" ><img src={logoIcon} alt="icon" className="nav-bar-icon" /></Link>
             <div className="search-bar">
-                <div className="enter-search" onClick={() => props.enteredSearch()}>
+                <div className="enter-search search-hover" onClick={() => props.enteredSearch()}>
                     <img src={searchIcon} className="search-icon" alt="search" />
                 </div>
-                <input className="search-input" type="text" onChange={(e) => props.onQueryChange(e)} onKeyPress={(e) => handleKeyPress(e)} />
-                <div className="searching-for">
+                <div className="search-hr" />
+                <input value={props.query} className="search-input" type="text" onChange={(e) => props.onQueryChange(e)} onKeyPress={(e) => handleKeyPress(e)} />
+                <div className="search-hr" />
+                <div className="searching-for search-hover">
                     all
                 </div>
             </div>
